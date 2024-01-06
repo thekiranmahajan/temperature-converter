0219052c90ab885462f5fa8f85f72edacc2f0a0d
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-06 13:52:30 +0530

    fixed formulas and decimal is limited to 2 points🌡️

--- index.js
+++ index.js
@@ -6,28 +6,26 @@ const kelvinEl = document.getElementById("kelvin");
 inputs.forEach((input) => {
   input.addEventListener("change", (e) => {
     if (e.target.value !== "") {
-      const temp = +e.target.value;
-      getConvertedTemp(e.target, temp);
+      const userInput = +e.target.value;
+      getConverteduserInput(e.target, userInput);
     }
   });
 });
 
-function getConvertedTemp(target, temp) {
+function getConverteduserInput(target, userInput) {
   switch (target.id) {
     case "celsius":
-      fehrenheitEl.value = (temp * 9) / 5 + 32;
-      kelvinEl.value = temp + 273.15;
-      console.log(kelvinEl.value);
+      fehrenheitEl.value = (userInput * 1.8 + 32).toFixed(2);
+      kelvinEl.value = (userInput + 273.15).toFixed(2);
       break;
     case "fahrenheit":
-      celsiusEl.value = ((temp - 32) * 5) / 9;
-      kelvinEl.value = ((temp - 32) * 5) / 9 + 273.15;
+      celsiusEl.value = ((userInput - 32) * (5 / 9)).toFixed(2);
+      kelvinEl.value = ((userInput - 32) * (5 / 9) + 273.15).toFixed(2);
       break;
     case "kelvin":
-      celsiusEl.value = temp - 273.15;
-      fehrenheitEl.value = ((temp - 273.15) * 9) / 5 + 32;
+      celsiusEl.value = (userInput - 273.15).toFixed(2);
+      fehrenheitEl.value = ((userInput - 273.15) * 1.8 + 32).toFixed(2);
       break;
-
     default:
       break;
   }
